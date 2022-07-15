878ec1b03d7400f9ca627b429128204c2887091e
RomildoDias <rdc.dias.c@gmail.com>
2022-07-15 20:16:34 -0300

    inicio do projeto em Angula-DIO

---
+++ course-manager/src/app/courses/course-list.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from "@angular/core";
+import { Course } from "./course";
+
+@Component({
+    selector:'app-course-list',
+    templateUrl:'./course-list.component.html'
+})
+export class CourseListComponent implements OnInit{
+    courses: Course[] = [];
+
+    ngOnInit{
+        this.course[]
+    }
+}[]
---
+++ course-manager/src/app/courses/course.ts
@@ -0,0 +1,10 @@
+export class Course{
+    id: number;
+    nome: string;
+    imagemUrl: string;
+    price: number;
+    code: string;
+    duration: number;
+    rating: number;
+
+}
