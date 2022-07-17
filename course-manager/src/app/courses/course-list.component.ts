72c06635f06d4bed387b1214133157eacdf88f7d
RomildoDias <rdc.dias.c@gmail.com>
2022-07-17 10:10:27 -0300

    criado o rating e add fontawesome para usar a classe para mostra as estrelas

--- course-manager/src/app/courses/course-list.component.ts
+++ course-manager/src/app/courses/course-list.component.ts
@@ -8,7 +8,28 @@ import { Course } from "./course";
 export class CourseListComponent implements OnInit{
     courses: Course[] = [];
 
-    ngOnInit{
-        this.course[]
+    ngOnInit(): void{
+        this.courses = [{
+            id: 1,
+            nome: 'Angulaev: forms',
+            imagemUrl: './assets/images/forms.png',
+            price: 99.99,
+            code: 'xps-1425',
+            duration: 120,
+            rating: 4.4,
+            releseDate:'Dezembro, 3, 2022'
+        
+        },
+        {
+            id: 2,
+            nome: 'Angulaev: http',
+            imagemUrl: './assets/images/http.png',
+            price: 97.99,
+            code: 'lps-1225',
+            duration: 60,
+            rating: 4,
+            releseDate:'dezembro, 4, 2022'
+        
+        }]
     }
 }[]
--- course-manager/src/app/courses/course.ts
+++ course-manager/src/app/courses/course.ts
@@ -6,5 +6,17 @@ export class Course{
     code: string;
     duration: number;
     rating: number;
+    releseDate: string;
+
+    constructor(id:number, nome:string, imagemUrl:string, price: number,code: string,duration: number, rating: number, relesesdate: string ){
+        this.id = id,
+        this.nome = nome;
+        this.imagemUrl = imagemUrl,
+        this.price = price;
+        this.code = code,
+        this.duration = duration;
+        this.rating = rating,
+        this.releseDate = relesesdate
+    }
 
 }
